567dc137576fc9fbdcbd41494d3a6e6edf48447c
kameranis <kameranis@gmail.com>
2020-01-04 17:36:15 -0600

    Change cutexp and cutweird to accept two (possibly overlapping) communities.

--- osvv/final/cutexp.c
+++ osvv/final/cutexp.c
@@ -20,92 +20,95 @@ mexFunction INPUTS;
 #include "mex.h"
 #include "matrix.h"
 
-void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
-{
-  const mxArray* G;
-  mwIndex* col;
-  mwIndex* row;
-  
-  double* array_G;
-  long* cut;
-  long size_cut;
-  long i, j;
-  long n;
-  double cutedges = 0;
-  
-  int* mask_cut; 
-  mxArray* exp;
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+    const mxArray *G;
+    mwIndex *col;
+    mwIndex *row;
 
-  mwSize dims[]={1,1}; 
-  long* temp;
+    double *array_G;
+    long *cut;
+    long *reciprocal_cut;
+    long size_cut;
+    long reciprocal_size_cut;
+    long i, j;
+    long n;
+    long lamda;
+    double cutedges = 0;
 
-  /*%%%%%%%%%%%%%%%%% ARGUMENT LOADING &  CHECKING %%%%%%%%%%%%%%%%%%%%%*/
+    int *mask_cut;
+    int *reciprocal_mask_cut;
+    mxArray *exp;
 
-  /* CHECK CORRECT NUMBER OF INPUT/OUTPUTS */
-  if(nrhs != 2 || nlhs != 3) 
-    mexErrMsgTxt("Error in cutexp. Incorrect usage.\n");
- 
-  /* CHECK TYPES */
-  if(!mxIsSparse(prhs[0]))
-    mexErrMsgTxt("Error in cutexp. Graph must be sparse.\n");
+    mwSize dims[] = {1, 1};
+    long *temp;
 
-  if(mxGetClassID(prhs[1]) != mxINT64_CLASS)
-    mexErrMsgTxt("Error in cutexp. Cut must be of class int64.\n");
-  
-  
-  /* LOAD ARGUMENTS */
-  G = prhs[0];
-  n = mxGetM(G);
-  col = mxGetJc(G);
-  row = mxGetIr(G);
-  array_G = mxGetPr(G);
+    /*%%%%%%%%%%%%%%%%% ARGUMENT LOADING &  CHECKING %%%%%%%%%%%%%%%%%%%%%*/
 
-  cut = (long*) mxGetPr(prhs[1]);
-  size_cut = mxGetM(prhs[1]);
- 
-  /* PREPARE MASK - COMPLEMENT CUT IF NECESSARY */
-  mask_cut = calloc(sizeof(*mask_cut), n+1);
-  if (mask_cut == NULL)
-    mexErrMsgTxt("Error allocating memory in cutexp.");
+    /* CHECK CORRECT NUMBER OF INPUT/OUTPUTS */
+    if (nrhs != 4 || nlhs != 3)
+        mexErrMsgTxt("Error in cutexp. Incorrect usage.\n");
 
-  
-  for(i=0; i < size_cut; i++)
-      mask_cut[cut[i]] = 1;
- 
+    /* CHECK TYPES */
+    if (!mxIsSparse(prhs[0]))
+        mexErrMsgTxt("Error in cutexp. Graph must be sparse.\n");
 
-  if(size_cut > n/2)    /* COMPLEMENTATION */
-    {
-    for(i=1; i < n+1 ; i++)
-      mask_cut[i] = !mask_cut[i];
+    if (mxGetClassID(prhs[2]) != mxINT64_CLASS)
+        mexErrMsgTxt("Error in cutexp. Cut must be of class int64.\n");
 
-    size_cut = n - size_cut;
+    if (mxGetClassID(prhs[3]) != mxINT64_CLASS)
+        mexErrMsgTxt("Error in cutexp. Reciprocal cut must be of class int64.\n");
 
-    j = 0;
-    for(i=1; i < n+1; i++)
-      {
-	if(mask_cut[i]) {
-	  cut[j] = i;
-	  j++;
-	}
-      } 
+
+    /* LOAD ARGUMENTS */
+    G = prhs[0];
+    n = mxGetM(G);
+    col = mxGetJc(G);
+    row = mxGetIr(G);
+    array_G = mxGetPr(G);
+    lamda = mxGetScalar(prhs[1]);
+
+    cut = (long *) mxGetPr(prhs[2]);
+    reciprocal_cut = (long *) mxGetPr(prhs[3]);
+    size_cut = mxGetM(prhs[2]);
+    reciprocal_size_cut = mxGetM(prhs[3]);
+
+    /* PREPARE MASK - COMPLEMENT CUT IF NECESSARY */
+    mask_cut = calloc(sizeof(*mask_cut), n + 1);
+    if (mask_cut == NULL)
+        mexErrMsgTxt("Error allocating memory in cutexp.");
+
+    reciprocal_mask_cut = calloc(sizeof(*reciprocal_mask_cut), n + 1);
+    if (reciprocal_mask_cut == NULL)
+        mexErrMsgTxt("Error allocating memory in cutexp.");
+
+
+    for (i = 0; i < size_cut; i++)
+        mask_cut[cut[i]] = 1;
+
+    for (i = 0; i < reciprocal_size_cut; i++)
+        reciprocal_mask_cut[reciprocal_cut[i]] = 1;
+
+    /* COMPUTE EXPANSION */
+    for (i = 0; i < size_cut; i++) {
+        if (reciprocal_mask_cut[cut[i]] == 1)
+            continue;
+        for (j = col[cut[i] - 1]; j < col[cut[i]]; j++) {
+            if (mask_cut[row[j] + 1] == 0)
+                cutedges += array_G[j];
+        }
     }
-  
+    cutedges += (size_cut + reciprocal_size_cut - n);
 
-  /* COMPUTE EXPANSION */
-  for(i=0; i < size_cut; i++)
-      for(j = col[cut[i]-1]; j < col[cut[i]]; j++) 
-	  if(mask_cut[row[j] + 1] == 0)
-	    cutedges += array_G[j];
+    /*%%%%%%%%%%%%%%%%%%%%% TERMINATION AND CLEANING %%%%%%%%%%%%%%%%%%%%%%%%*/
+    if (size_cut > reciprocal_size_cut)
+        size_cut = reciprocal_size_cut;
+    plhs[0] = mxCreateDoubleScalar(cutedges);
+    plhs[1] = mxCreateNumericArray(2, dims, mxINT64_CLASS, mxREAL);
+    plhs[2] = mxCreateDoubleScalar(cutedges / size_cut);
+    temp = (long *) mxGetPr(plhs[1]);
+    temp[0] = size_cut;
 
-  /*%%%%%%%%%%%%%%%%%%%%% TERMINATION AND CLEANING %%%%%%%%%%%%%%%%%%%%%%%%*/
-  
-  plhs[0] = mxCreateDoubleScalar(cutedges); 
-  plhs[1] = mxCreateNumericArray(2, dims, mxINT64_CLASS, mxREAL);
-  plhs[2] = mxCreateDoubleScalar(cutedges/size_cut);  
-  temp = (long*) mxGetPr(plhs[1]);
-  temp[0] = size_cut; 
-
-  free(mask_cut);
+    free(mask_cut);
 
 } 
 
--- osvv/final/cutweird.c
+++ osvv/final/cutweird.c
@@ -20,99 +20,131 @@ mexFunction INPUTS;
 #include "mex.h"
 #include "matrix.h"
 
-void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
-{
-  const mxArray* G;
-  mwIndex* col;
-  mwIndex* row;
-  
-  double* array_G;
-  long* cut;
-  long size_cut;
-  long* bisec;
-  long size_bisec;
-  long i, j;
-  long n;
-  double cutedges = 0;
-  long size_intersect;
-  long denominator;
-  
-  int* mask_cut; 
-  mxArray* exp;
+#define abs(x) (x) > 0 ? (x) : -(x)
 
-  mwSize dims[]={1,1}; 
-  long* temp;
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+    const mxArray *G;
+    mwIndex *col;
+    mwIndex *row;
 
-  /*%%%%%%%%%%%%%%%%% ARGUMENT LOADING &  CHECKING %%%%%%%%%%%%%%%%%%%%%*/
+    double *array_G;
+    long *cut;
+    long *reciprocal_cut;
+    long size_cut;
+    long reciprocal_size_cut;
+    long *bisec;
+    long size_bisec;
+    long i, j;
+    long n;
+    double cutedges = 0;
+    long size_intersect;
+    long reciprocal_size_intersect;
+    long denominator;
+    long lamda;
 
-  /* CHECK CORRECT NUMBER OF INPUT/OUTPUTS */
-  if(nrhs != 3 || nlhs != 3) 
-    mexErrMsgTxt("Error in cutweird. Incorrect usage.\n");
- 
-  /* CHECK TYPES */
-  if(!mxIsSparse(prhs[0]))
-    mexErrMsgTxt("Error in cutweird. Graph must be sparse.\n");
+    int *mask_cut;
+    int *reciprocal_mask_cut;
+    mxArray *exp;
 
-  if(mxGetClassID(prhs[1]) != mxINT64_CLASS)
-    mexErrMsgTxt("Error in cutweird. Cut must be of class int64.\n");
+    mwSize dims[] = {1, 1};
+    long *temp;
 
-  if(mxGetClassID(prhs[2]) != mxINT64_CLASS)
-    mexErrMsgTxt("Error in cutweird. Bisec must be of class int64.\n");
-  
-  
-  /* LOAD ARGUMENTS */
-  G = prhs[0];
-  n = mxGetM(G);
-  col = mxGetJc(G);
-  row = mxGetIr(G);
-  array_G = mxGetPr(G);
+    /*%%%%%%%%%%%%%%%%% ARGUMENT LOADING &  CHECKING %%%%%%%%%%%%%%%%%%%%%*/
 
-  cut = (long*) mxGetPr(prhs[1]);
-  size_cut = mxGetM(prhs[1]);
- 
-  bisec = (long*) mxGetPr(prhs[2]);
-  size_bisec = mxGetM(prhs[2]);
+    /* CHECK CORRECT NUMBER OF INPUT/OUTPUTS */
+    if (nrhs != 5 || nlhs != 3)
+        mexErrMsgTxt("Error in cutweird. Incorrect usage.\n");
+
+    /* CHECK TYPES */
+    if (!mxIsSparse(prhs[0]))
+        mexErrMsgTxt("Error in cutweird. Graph must be sparse.\n");
+
+    if (mxGetClassID(prhs[1]) != mxINT64_CLASS)
+        mexErrMsgTxt("Error in cutweird. Cut must be of class int64.\n");
+
+    if (mxGetClassID(prhs[2]) != mxINT64_CLASS)
+        mexErrMsgTxt("Error in cutweird. Cut must be of class int64.\n");
+
+    if (mxGetClassID(prhs[3]) != mxINT64_CLASS)
+        mexErrMsgTxt("Error in cutweird. Bisec must be of class int64.\n");
 
 
-  /*%%%%%%%%%%%%%%%%%%%%%%%%% MAIN BODY %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
+    /* LOAD ARGUMENTS */
+    G = prhs[0];
+    n = mxGetM(G);
+    col = mxGetJc(G);
+    row = mxGetIr(G);
+    array_G = mxGetPr(G);
 
-  /* PREPARE CUT MASK  AND COMPUTE INTERSECT*/
-  mask_cut = calloc(sizeof(*mask_cut), n+1);
-  if (mask_cut == NULL)
-    mexErrMsgTxt("Error allocating memory in cutweird.");
+    cut = (long *) mxGetPr(prhs[1]);
+    size_cut = mxGetM(prhs[1]);
+    reciprocal_cut = (long *) mxGetPr(prhs[2]);
+    reciprocal_size_cut = mxGetM(prhs[2]);
 
-  for(i=0; i < size_cut; i++)
-      mask_cut[cut[i]] = 1;
+    bisec = (long *) mxGetPr(prhs[3]);
+    size_bisec = mxGetM(prhs[3]);
 
-  size_intersect = 0;
-  for(i=0; i < size_bisec; i++)
-    if(mask_cut[bisec[i]] == 1)
-      size_intersect++;
+    lamda = mxGetScalar(prhs[4]);
 
-  
 
-  /* COMPUTE WEIRD RATIO*/
-  /* COMPUTE EDGES CUT */
-  for(i=0; i < size_cut; i++)
-      for(j = col[cut[i]-1]; j < col[cut[i]]; j++) 
-	  if(mask_cut[row[j] + 1] == 0)
-	    cutedges += array_G[j];
-  
-  /* COMPUTE DENOMINATOR */
-  denominator = 2 * size_intersect - size_cut;
+    /*%%%%%%%%%%%%%%%%%%%%%%%%% MAIN BODY %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
 
-  if(denominator < 0)
-    denominator = denominator * (-1);
+    /* PREPARE CUT MASK  AND COMPUTE INTERSECT*/
+    mask_cut = calloc(sizeof(*mask_cut), n + 1);
+    if (mask_cut == NULL)
+        mexErrMsgTxt("Error allocating memory in cutweird.");
 
-  /*%%%%%%%%%%%%%%%%%%%%% TERMINATION AND CLEANING %%%%%%%%%%%%%%%%%%%%%%%%*/
-  
-  plhs[0] = mxCreateDoubleScalar(cutedges); 
-  plhs[1] = mxCreateNumericArray(2, dims, mxINT64_CLASS, mxREAL);
-  plhs[2] = mxCreateDoubleScalar(cutedges/denominator);  
-  temp = (long*) mxGetPr(plhs[1]);
-  *temp = denominator; 
+    for (i = 0; i < size_cut; i++)
+        mask_cut[cut[i]] = 1;
 
-  free(mask_cut);
+    reciprocal_mask_cut = calloc(sizeof(*reciprocal_mask_cut), n + 1);
+    if (reciprocal_mask_cut == NULL)
+        mexErrMsgTxt("Error allocating memory in cutweird.");
+
+    for (i = 0; i < reciprocal_size_cut; i++)
+        reciprocal_mask_cut[reciprocal_cut[i]] = 1;
+
+    size_intersect = 0;
+    for (i = 0; i < size_bisec; i++)
+        if (mask_cut[bisec[i]] == 1)
+            size_intersect++;
+
+    reciprocal_size_intersect = 0;
+    for (i = 0; i < size_bisec; i++)
+        if (reciprocal_mask_cut[bisec[i]] == 1)
+            reciprocal_size_intersect++;
+
+
+    /* COMPUTE WEIRD RATIO*/
+    /* COMPUTE EDGES CUT */
+    for (i = 0; i < size_cut; i++) {
+        if (reciprocal_mask_cut[cut[i]] == 1)
+            continue;
+        for (j = col[cut[i] - 1]; j < col[cut[i]]; j++)
+            if (mask_cut[row[j] + 1] == 0)
+                cutedges += array_G[j];
+    }
+
+    printf("cutedges: %lf\n", cutedges);
+    if (lamda > 0) cutedges += (size_cut + reciprocal_size_cut - n);
+    /* COMPUTE DENOMINATOR */
+    if (abs(2 * size_intersect - size_cut) < abs(2 * reciprocal_size_intersect - reciprocal_size_cut))
+        denominator = 2 * size_intersect - size_cut;
+    else
+        denominator = 2 * reciprocal_size_intersect - reciprocal_size_cut;
+
+    if (denominator < 0)
+        denominator = denominator * (-1);
+
+    /*%%%%%%%%%%%%%%%%%%%%% TERMINATION AND CLEANING %%%%%%%%%%%%%%%%%%%%%%%%*/
+
+    plhs[0] = mxCreateDoubleScalar(cutedges);
+    plhs[1] = mxCreateNumericArray(2, dims, mxINT64_CLASS, mxREAL);
+    plhs[2] = mxCreateDoubleScalar(cutedges / (double) denominator);
+    temp = (long *) mxGetPr(plhs[1]);
+    *temp = denominator;
+
+    free(mask_cut);
 
 } 
 
