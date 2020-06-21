f8e7612bb9c312a183fff621bb61b6a44d9383f9
kameranis <kameranis@gmail.com>
2020-06-21 17:06:46 -0500

    Fix memory leaks and dynamic memory allocation for matchings.
    Made sure all pointers are freed at the end of Pairing (thanks Satish).
    Instead of assuming the worst case scenario of each matching carrying
    exactly one unit of flow, turned mheads, mtails, mweights to dynamic
    vectors. This way even if there is a huge flow from a few paths it
    won't run out of memory (thanks Satish).

--- osvv/final/Pairing.c
+++ osvv/final/Pairing.c
@@ -29,200 +29,198 @@ OUTPUTS:
 function [flow, cut, matching]= Pairing(G, bisec, cap_add, cap_orig); 
 */
 
-void hipr 
-( 
-   long ninput, 
-   long minput, 
-   long *tails, 
-   long *heads, 
-   long *weights, 
-   long s, 
-   long t,
-   int **output_set,
-   long **mheads,
-   long **mtails,
-   long **mweights,
-   long* nedges,
-   long* fflow,
-   int route_flag
-);
+void hipr
+        (
+                long ninput,
+                long minput,
+                long *tails,
+                long *heads,
+                long *weights,
+                long s,
+                long t,
+                int **output_set,
+                long **mheads,
+                long **mtails,
+                long **mweights,
+                long *nedges,
+                long *fflow,
+                int route_flag
+        );
 
 
+mxArray *Sparse(long *heads, long *tails, long *weights, long m, long n);
 
-mxArray* Sparse(long* heads, long* tails, long* weights, long m, long n );
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+    const mxArray *G;
+    long *bisec;
+    long cap_add;
+    long cap_orig;
 
-void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
-{
-  const mxArray* G;
-  long* bisec;
-  long cap_add;
-  long cap_orig;
-  
-  long N;
-  long M;
-  long size_bisec;
-  long* col_G;
-  long* row_G;
-  double* pr_G;
-  
-  
-  long *tails;
-  long *heads;
-  long *weights;
-  
-
-  long i;
-  long j;
-  long k;
-  long h;
-  int* mask;
-  mxArray* temp;
-
-  long n;
-  long m;
-  int* output_set;
-  long* mheads;
-  long* mtails;
-  long* mweights;
-  long nedges;
-  long fflow;
-  long size_cut;
-
-  mxArray* matching;
-  mxArray* cut;
-  mxArray* flow;
-  long* cut_pr;
-
-  mwSize dims[] = {1,1};
-  long* temp_a;
-
-  float t1, t2;
-  int route_flag;
- 
-  /*  t1 =timer();*/
-
-  if(nrhs != 4 || nlhs > 3 || nlhs < 2)
-    mexErrMsgTxt("Error in usage of Pairing.\n");
-
-  /* EXTRACT DATA FROM MATLAB */
-  G = prhs[0];
-  bisec = (long*) mxGetPr(prhs[1]);
-  size_bisec = mxGetM(prhs[1]);
-  cap_add = ((long*) mxGetPr(prhs[2]))[0];
-  cap_orig = ((long*) mxGetPr(prhs[3]))[0];
-  
-  N = mxGetM(G);
-  mexCallMATLAB(1, &temp, 1, &G, "nnz");
-  M = mxGetScalar(temp);
- 
-  col_G = (long*) mxGetJc(G);
-  row_G = (long*) mxGetIr(G);
-  pr_G = (double*) mxGetPr(G);
-
-  
-  /* CONSTRUCT THE FLOW PROBLEM IN THW REPRESENTATION 
-     NEED TO ADD SOURCE / SINK AND RELATIVE EDGES
-  */
-  
-  tails = calloc(sizeof(*tails), M + N);
-  heads = calloc(sizeof(*heads), M + N);
-  weights = calloc(sizeof(*weights), M + N);
-  mask = calloc(sizeof(*mask), N+1);
-
-  for(h=0; h<size_bisec; h++)
-    mask[bisec[h]]= 1;
-  
-  k = 0;
+    long N;
+    long M;
+    long size_bisec;
+    long *col_G;
+    long *row_G;
+    double *pr_G;
 
 
-  for(i=0; i < N; i++)
-    for(j=col_G[i]; j < col_G[i+1]; j++)
-      {
-	heads[k] = i+1;
-	tails[k] = row_G[j]+1;
-	weights[k] = pr_G[j] * cap_orig;
+    long *tails;
+    long *heads;
+    long *weights;
 
-	k++;
-      }
 
-  
-  for(h=0; h < N; h++) 
-    {
-      if(mask[h+1] == 1) 
-	{
-	  heads[k] = h+1;
-	  tails[k] = N+1;
-	}
-      else
-	{
-	  heads[k] = N+2;
-	  tails[k] = h+1;
-	}
-      weights[k] = cap_add;
-      
-      k++;
+    long i;
+    long j;
+    long k;
+    long h;
+    int *mask;
+    mxArray *temp;
+
+    long n;
+    long m;
+    int *output_set;
+    long *mheads = NULL;
+    long *mtails = NULL;
+    long *mweights = NULL;
+    long nedges = NULL;
+    long fflow;
+    long size_cut;
+
+    mxArray *matching;
+    mxArray *cut;
+    mxArray *flow;
+    long *cut_pr;
+
+    mwSize dims[] = {1, 1};
+    long *temp_a;
+
+    float t1, t2;
+    int route_flag;
+
+    /*  t1 =timer();*/
+
+    if (nrhs != 4 || nlhs > 3 || nlhs < 2)
+        mexErrMsgTxt("Error in usage of Pairing.\n");
+
+    /* EXTRACT DATA FROM MATLAB */
+    G = prhs[0];
+    bisec = (long *) mxGetPr(prhs[1]);
+    size_bisec = mxGetM(prhs[1]);
+    cap_add = ((long *) mxGetPr(prhs[2]))[0];
+    cap_orig = ((long *) mxGetPr(prhs[3]))[0];
+
+    N = mxGetM(G);
+    mexCallMATLAB(1, &temp, 1, &G, "nnz");
+    M = mxGetScalar(temp);
+
+    col_G = (long *) mxGetJc(G);
+    row_G = (long *) mxGetIr(G);
+    pr_G = (double *) mxGetPr(G);
+
+
+    /* CONSTRUCT THE FLOW PROBLEM IN THW REPRESENTATION
+       NEED TO ADD SOURCE / SINK AND RELATIVE EDGES
+    */
+
+    tails = calloc(sizeof(*tails), M + N);
+    heads = calloc(sizeof(*heads), M + N);
+    weights = calloc(sizeof(*weights), M + N);
+    mask = calloc(sizeof(*mask), N + 1);
+
+    for (h = 0; h < size_bisec; h++)
+        mask[bisec[h]] = 1;
+
+    k = 0;
+
+
+    for (i = 0; i < N; i++)
+        for (j = col_G[i]; j < col_G[i + 1]; j++) {
+            heads[k] = i + 1;
+            tails[k] = row_G[j] + 1;
+            weights[k] = pr_G[j] * cap_orig;
+
+            k++;
+        }
+
+
+    for (h = 0; h < N; h++) {
+        if (mask[h + 1] == 1) {
+            heads[k] = h + 1;
+            tails[k] = N + 1;
+        } else {
+            heads[k] = N + 2;
+            tails[k] = h + 1;
+        }
+        weights[k] = cap_add;
+
+        k++;
     }
-  
-
-  /* CALL HI_PR - modified to output flow - would prefer for hipr to allocate this memory*/
 
 
-  /* m-arrays are initialized within hi_pr */
-  n = N+2;
-  m = M+N;
-
-  if(nlhs == 2)
-    {
-      route_flag = 0;
-    }
-  else
-    route_flag = 1;
+    /* CALL HI_PR - modified to output flow - would prefer for hipr to allocate this memory*/
 
 
-  /*  t1 = timer() - t1;*/
-  hipr(n, m, tails, heads, weights, N+1, N+2, &output_set, &mheads, &mtails, &mweights, &nedges, &fflow, route_flag);
-  /*  t2 = timer();*/
-  
+    /* m-arrays are initialized within hi_pr */
+    n = N + 2;
+    m = M + N;
+
+    if (nlhs == 2) {
+        route_flag = 0;
+    } else
+        route_flag = 1;
 
 
-  
-  /* INITIALIZE MATCHING */
-  if(route_flag == 1)
-    matching = Sparse(mheads, mtails, mweights, nedges, N);
-   
-   /* INITIALIZE FLOW */
-  flow = mxCreateNumericArray(2, dims, mxINT64_CLASS, mxREAL);
-  temp_a = (long*) mxGetPr(flow);
-  *temp_a = fflow;  
-  
+    /*  t1 = timer() - t1;*/
+    hipr(n, m, tails, heads, weights, N + 1, N + 2, &output_set, &mheads, &mtails, &mweights, &nedges, &fflow,
+         route_flag);
+    /*  t2 = timer();*/
 
-   /* INITIALIZE CUT - recall cut returned is sink side! so need to complement*/
-   size_cut = 0;
-   for(i=0; i < N; i++)
-     if(output_set[i] != 0)
-       size_cut++;
-   
-   size_cut = N - size_cut;
 
-   k = 0;
-   dims[0]=size_cut;
-   cut = mxCreateNumericArray(2,dims,mxINT64_CLASS, mxREAL);
-   cut_pr = (long*) mxGetPr(cut);
 
-   for(i=0; i< N; i++)
-     if(output_set[i] == 0) 
-       {
-	 cut_pr[k]= (long) i+1;
-	 k++;
-       }
 
-   plhs[0] = flow;
-   plhs[1] = cut;
-  
-   if(route_flag == 1)
-     plhs[2] = matching;
+    /* INITIALIZE MATCHING */
+    if (route_flag == 1)
+        matching = Sparse(mheads, mtails, mweights, nedges, N);
 
-   /*   t2 = timer() -t2;
-	fprintf(stderr, "Oth tm: %f", t2 + t1);*/
+    /* INITIALIZE FLOW */
+    flow = mxCreateNumericArray(2, dims, mxINT64_CLASS, mxREAL);
+    temp_a = (long *) mxGetPr(flow);
+    *temp_a = fflow;
+
+
+    /* INITIALIZE CUT - recall cut returned is sink side! so need to complement*/
+    size_cut = 0;
+    for (i = 0; i < N; i++)
+        if (output_set[i] != 0)
+            size_cut++;
+
+    size_cut = N - size_cut;
+
+    k = 0;
+    dims[0] = size_cut;
+    cut = mxCreateNumericArray(2, dims, mxINT64_CLASS, mxREAL);
+    cut_pr = (long *) mxGetPr(cut);
+
+    for (i = 0; i < N; i++)
+        if (output_set[i] == 0) {
+            cut_pr[k] = (long) i + 1;
+            k++;
+        }
+
+    plhs[0] = flow;
+    plhs[1] = cut;
+
+    if (route_flag == 1)
+        plhs[2] = matching;
+
+    /*   t2 = timer() -t2;
+     fprintf(stderr, "Oth tm: %f", t2 + t1);*/
+    free(heads);
+    free(tails);
+    free(weights);
+    if (mheads) free(mheads);
+    if (mtails) free(mtails);
+    if (mweights) free(mweights);
+    free(output_set);
 }
  
