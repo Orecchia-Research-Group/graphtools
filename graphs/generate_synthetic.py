548b778dcd929c0fd67ba51a536ac6bfa52c9bf3
Konstantinos Ameranis <kameranis@gmail.com>
2020-12-04 15:19:46 -0600

    Change synthetic graph generation.

--- graphs/generate_synthetic.py
+++ graphs/generate_synthetic.py
@@ -6,7 +6,7 @@ import pandas as pd
 from itertools import product
 np.random.seed(0)
 
-def create_graph(nodes, cluster_prob, p, q, r, e):
+def create_graph(nodes, cluster_prob, p, q, e):
     """Create a SBM graph of size `nodes` with probability of
     belonging to L, R, C according to `cluster_probs` and
     edge probability between them proportional to
@@ -24,31 +24,31 @@ def create_graph(nodes, cluster_prob, p, q, r, e):
 
     gmean = np.sqrt(np.outer(cluster_sizes, cluster_sizes))
 
-    probability_matrix = np.array([[p, e, q], [e, p, q], [q, q, r]]) * np.log(gmean) / gmean
+    probability_matrix = np.array([[p, e, p], [e, q, q], [p, q, p + q]]) * np.log(gmean) / (2 * gmean)
     print(probability_matrix)
     G = nx.stochastic_block_model(cluster_sizes, probability_matrix)
     return G
 
 
-nodes = 10000
+nodes = [10000, 30000, 100000, 300000, 1000000]
 e = 0.005
-for cluster_probs in np.array([(0.49, 0.49, 0.02), (0.74, 0.24, 0.02)]):
-    for p, q, r in [(1, 1, 0.2), (1, 1, 0.5), (1, 1, 1), (1, 1, 2), (1, 1, 3), (3, 2, 1), (3, 2, 2), (3, 2, 4), (3, 2, 6), (3, 4, 1), (3, 4, 3), (3, 4, 5), (3, 4, 6), (5, 3, 0.2), (10, 4, 0.4)]:
-        while True:
-            G = create_graph(nodes, cluster_probs, p, q, r, e)
-            n = nx.number_of_nodes(G)
-            m = nx.number_of_edges(G)
-            conn = nx.is_connected(G)
-            print(p, q, r, m, conn)
-            if not conn:
-                continue
-            prefix = "synthetic_graph_{:.0f}_{:.0f}_{:.0f}_{:.0f}_{:.0f}".format(p*10, q*10, r*10, *cluster_probs * 100)
-            with open("synthetic/{}.eg2".format(prefix), "w") as f_out:
-                print(n, n, 2*m, file=f_out)
-                for v in G.nodes:
-                    for _, u in G.edges(v, data=False):
-                        print(v, u, 1, file=f_out)
-            edgelistFile = "synthetic/{}.edgelist".format(prefix)
-            nx.write_edgelist(G, edgelistFile, data=False, delimiter='\t')
-            break
+for n in nodes:
+    for cluster_probs in np.array([(0.49, 0.49, 0.02), (0.74, 0.24, 0.02)]):
+        for p, q in [(2, 2), (3, 2), (3, 4), (5, 3), (10, 4)]:
+            for i in range(3):
+                G = create_graph(n, cluster_probs, p, q, e)
+                m = nx.number_of_edges(G)
+                conn = nx.is_connected(G)
+                print(p, q, n, m, conn)
+                if not conn:
+                    continue
+                prefix = "synthetic_graph_{:d}_{:d}_{:d}_{:.0f}_{:.0f}".format(n, p, q, *cluster_probs * 100)
+                with open("synthetic/{}.eg2".format(prefix), "w") as f_out:
+                    print(n, n, 2*m, file=f_out)
+                    for v in G.nodes:
+                        for _, u in G.edges(v, data=False):
+                            print(v, u, 1, file=f_out)
+                edgelistFile = "synthetic/{}.edgelist".format(prefix)
+                nx.write_edgelist(G, edgelistFile, data=False, delimiter='\t')
+                break
 
