bc0399de8ab59299d1ab9a6a6f4e93c48e302f5a
kameranis <kameranis@gmail.com>
2021-01-25 22:48:52 +0200

    Add eg2tometis.
    Converts all .eg2 files to their .metis equivalent.

---
+++ osvv/final/eg2tometis.m
@@ -0,0 +1,32 @@
+function [] = eg2tometis(inputDirectory)
+%EG2TOMETIS Summary of this function goes here
+%   Detailed explanation goes here
+
+files = dir(fullfile(inputDirectory, '*.eg2'));
+for f=1:length(files)
+    [~, dataset, ~] = fileparts(files(f).name);
+    fprintf('Processing %s...\n', dataset);
+    [G, n, m] = loadeg2graph(fullfile(files(f).folder, files(f).name));
+    outfile = fullfile(inputDirectory, sprintf('%s.metis', dataset));
+    fOut = fopen(outfile, 'w');
+    weight = int64(full(sum(G)));
+    [column, row, value] = find(G);
+    fprintf(fOut, '%d %d 011\n', n, m/2);
+    fprintf(fOut, '%d', weight(1));
+    prev_row = 1;
+    for i=1:length(value)
+        % Print degree
+        if row(i) ~= prev_row
+            for k=prev_row+1:row(i)
+                fprintf(fOut, '\n%d', weight(k));
+            end
+            prev_row = row(i);
+        end
+        
+        % Print current edge
+        fprintf(fOut, ' %d %d', column(i), value(i));
+    end
+end
+
+end
+
