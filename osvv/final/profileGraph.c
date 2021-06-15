32439201498a96f6681dc00c938ca0c537ec39db
Andrew Razborov <arazborov@ucls.uchicago.edu>
2021-06-14 21:03:26 -0500

    Small formatting change in error message

--- osvv/final/profileGraph.c
+++ osvv/final/profileGraph.c
@@ -4,7 +4,7 @@
 
 int main (int argc, char *argv[]) {
     if((argc < 4) || (argc > 6)){
-        fprintf(stderr, "Usage: %s eg2Path, ptnPath, alphaPath, runNumber, algorithm", argv[0]);
+        fprintf(stderr, "Usage: %s eg2Path, ptnPath, alphaPath, [runNumber, [algorithm]]\n", argv[0]);
         exit(-1);
     }
 
