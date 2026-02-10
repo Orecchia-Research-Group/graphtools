3bacb4a1c82641ba86413e7c58d0864314b65b21
Konstantinos Ameranis <kameranis@gmail.com>
2026-02-10 19:14:22 +0000

    Update graph loader and .gitignore

--- osvv/final/cutfind.m
+++ osvv/final/cutfind.m
@@ -102,17 +102,9 @@ tic;
 rand('twister', seed);
 
 % READ GRAPH G
-if(ischar(fileToRead))
-    [G, weight] = loadMetisGraph(fileToRead);
-    n = size(G, 1);
-    weight = int64(weight);
-else
-    G = fileToRead;
-    n = size(G, 1);
-    degree = int64(full(sum(G)));
-    weight = ones(1, n, 'int64');
-    weight(:) = degree;
-end
+[G, weight] = loadGraph(fileToRead);
+n = size(G, 1);
+
 % ERROR CHECKING: G MUST BE UNDIRECTED
 % if(nnz(G - G') ~= 0)
 %    error('The eg2 graph is not undirected.\n');
@@ -389,7 +381,7 @@ for i=1:double(t)
         
         % PRINT RUN RESULTS TO OUTPUT FILE
         % fprintf(output(stop_cnt), 'r:\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n', 'seed', 'minexp', 'minexp_num', 'minexp_den', 'endtime', 'inittime', 'i', 'lower', 'flownumber', 'spectime', 'flowtime', 'lowertime');
-        fprintf(output(stop_cnt), 'r:\t%d\t%f\t%d\t%d\t%f\t%f\t%d\t%f\t%d\t%f\t%f\t%f\t%d\n', seed, minexp, minexp_num, minexp_den, endtime, inittime, i, lower, flownumber, spectime, flowtime, lowertime, nnz(H));
+        fprintf(output(stop_cnt), 'r:\t%d\t%.8f\t%d\t%d\t%f\t%f\t%d\t%.8f\t%d\t%f\t%f\t%f\t%d\n', seed, minexp, minexp_num, minexp_den, endtime, inittime, i, lower, flownumber, spectime, flowtime, lowertime, nnz(H));
         
         if(~strcmp(lwbd, 'n') && verbose > 0)
             fprintf(2,'Lower bound: %f.\n', lower);
